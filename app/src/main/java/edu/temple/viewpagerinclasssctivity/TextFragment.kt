1f0c6c31721d8f6222f2a82e5cd8e98670ccc33e
Harris Kwong <harriskwong120898@gmail.com>
2023-04-04 12:13:42 -0400

    Completed activity.

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/TextFragment.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/TextFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +20,9 @@ class TextFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
 
+    val textView : TextView by lazy{
+        requireView().findViewById(R.id.textView)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,6 +38,12 @@ class TextFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_text, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        param1?.run{
+            textView.text = this
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
