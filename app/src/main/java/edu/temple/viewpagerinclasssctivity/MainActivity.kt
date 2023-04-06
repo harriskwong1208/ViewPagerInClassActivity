77ab3449cc8692099676b5e2c00f12e00afdc27f
Harris Kwong <harriskwong120898@gmail.com>
2023-04-06 11:48:04 -0400

    Placed button in layout

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -12,9 +13,20 @@ class MainActivity : AppCompatActivity() {
     val viewPager: ViewPager2 by lazy{
         findViewById(R.id.viewPager)
     }
+
+    val button: Button by lazy{
+        findViewById(R.id.button)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+
+
+        }
+
 
         viewPager.adapter = object: FragmentStateAdapter(this){
             override fun getItemCount() = 10
